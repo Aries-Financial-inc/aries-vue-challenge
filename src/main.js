ebcfcfa312d0d7b45f2af61518ef2f8a49455f4a
profsam97 <proftoby4real@hotmail.com>
2024-06-20 14:02:36 +0100

    added notification

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './index.css'
+import Notifications from 'vue-notification';
 
 Vue.config.productionTip = false
+
+Vue.use(Notifications)
 
 
 
