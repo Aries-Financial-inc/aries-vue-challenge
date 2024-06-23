b12a2001a46709ebc21c65714ecd884024a43b50
Rushikesh Gandhmal <rushikeshgandhmal@gmail.com>
2024-06-23 21:16:47 +0530

    folder structure improve

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { ThemeProvider } from "./context/ThemeContext";
+import { ThemeProvider } from "../context/ThemeContext";
 import { UNDERLYING_PRICES } from "../constants";
 import {
   AnalysisResult,
