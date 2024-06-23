929ed6d640749741923331b6cc98b68f2db1df9d
Rushikesh Gandhmal <rushikeshgandhmal@gmail.com>
2024-06-23 11:56:30 +0530

    add Table

---
+++ src/components/Table.tsx
@@ -0,0 +1,28 @@
+import { AnalysisResult } from "../../types/options";
+
+interface TableProps {
+  data: AnalysisResult[];
+}
+
+const Table: React.FC<TableProps> = ({ data }) => {
+  return (
+    <table className="table-auto w-full mt-4">
+      <thead>
+        <tr>
+          <th>Price at Expiry</th>
+          <th>Profit/Loss</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((row, index) => (
+          <tr key={index}>
+            <td className="border px-4 py-2">{row.priceAtExpiry}</td>
+            <td className="border px-4 py-2">{row.profitLoss}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default Table;
