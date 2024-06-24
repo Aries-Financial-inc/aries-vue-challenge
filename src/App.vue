ae314dfc9fd972e53e19cba9167c79e5db65e645
Danh Luong <danh.lt@raksul.com>
2024-06-24 20:34:54 +0700

    setup env with typescript and jest

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <template>
   <div id="app">
-    <CodingChallenge :optionsData="sampleData"/>
+    <CodingChallenge :optionsData="sampleData" />
   </div>
 </template>
 
-<script>
+<script lang="ts">
+import Vue from 'vue'
 import CodingChallenge from './components/CodingChallenge.vue'
 
-export default {
+export default Vue.extend({
   name: 'App',
   components: {
     CodingChallenge
@@ -16,41 +17,41 @@ export default {
     return {
       sampleData: [
         {
-          "strike_price": 100, 
-          "type": "Call", 
-          "bid": 10.05, 
-          "ask": 12.04, 
-          "long_short": "long", 
-          "expiration_date": "2025-12-17T00:00:00Z"
+          strike_price: 100,
+          type: 'Call',
+          bid: 10.05,
+          ask: 12.04,
+          long_short: 'long',
+          expiration_date: '2025-12-17T00:00:00Z'
         },
         {
-          "strike_price": 102.50, 
-          "type": "Call", 
-          "bid": 12.10, 
-          "ask": 14, 
-          "long_short": "long", 
-          "expiration_date": "2025-12-17T00:00:00Z"
+          strike_price: 102.5,
+          type: 'Call',
+          bid: 12.1,
+          ask: 14,
+          long_short: 'long',
+          expiration_date: '2025-12-17T00:00:00Z'
         },
         {
-          "strike_price": 103, 
-          "type": "Put", 
-          "bid": 14, 
-          "ask": 15.50, 
-          "long_short": "short", 
-          "expiration_date": "2025-12-17T00:00:00Z"
+          strike_price: 103,
+          type: 'Put',
+          bid: 14,
+          ask: 15.5,
+          long_short: 'short',
+          expiration_date: '2025-12-17T00:00:00Z'
         },
         {
-          "strike_price": 105, 
-          "type": "Put", 
-          "bid": 16, 
-          "ask": 18, 
-          "long_short": "long", 
-          "expiration_date": "2025-12-17T00:00:00Z"
+          strike_price: 105,
+          type: 'Put',
+          bid: 16,
+          ask: 18,
+          long_short: 'long',
+          expiration_date: '2025-12-17T00:00:00Z'
         }
       ]
     }
   }
-}
+})
 </script>
 
 <style>
--- src/components/CodingChallenge.vue
+++ src/components/CodingChallenge.vue
@@ -1,18 +1,20 @@
 <template>
   <div>
     <h1>Options Profit Calculator</h1>
-    {{ "Your Code Here" }}
+    {{ 'Your Code Here' }}
   </div>
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
   name: 'CodingChallenge',
   props: {
     optionsData: Array
   }
   // Your code here
-}
+})
 </script>
 
 <style scoped>
